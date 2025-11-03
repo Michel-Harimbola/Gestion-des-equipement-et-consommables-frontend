158cac2792b34e745537b22963dce76c8e300181
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-03 19:05:35 +0300

    feat: LoadingContextHandler: setLoadingInstance, getLoadingInstance

---
+++ src/context/LoadingContextHandler.jsx
@@ -0,0 +1,9 @@
+let loadingInstance = null;
+
+export const setLoadingInstance = (instance) => {
+    loadingInstance = instance;
+};
+
+export const getLoadingInstance = () => {
+    loadingInstance;
+}
