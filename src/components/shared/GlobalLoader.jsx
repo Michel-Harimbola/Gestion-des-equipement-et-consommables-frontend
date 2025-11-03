10236972737aabebd5695af60f8401d3ae4ad132
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-03 21:56:58 +0300

    feat: GlobalLoader.jsx

---
+++ src/components/shared/GlobalLoader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { useLoading } from "../../context/LoadingContext";
+
+export default function GlobalLoader() {
+  const { isLoading } = useLoading();
+  if (!isLoading) return null;
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
+      <div className="w-16 h-16 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+    </div>
+  );
+}
