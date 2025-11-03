4dc0eb01166c6d6ab9e4f3b4f8fa6e6a1753c118
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-03 20:55:21 +0300

    feat: Login.jsx: envoie les données au authSlice

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,27 @@
 import React, { useState } from 'react'
+import { useDispatch } from 'react-redux';
+import { loginUser } from "../redux/slices/authSlice";
+import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 import { LogIn as LogInIcon, Lock as LockIcon, Mail as MailIcon, Eye, EyeOff } from 'lucide-react';
 
-const Login = () => {
+export default function Login() {
+  const [form, setForm] = useState({ email: "", password: "" });
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await dispatch(loginUser(form)).unwrap();
+      toast.success("Login successful!");
+      navigate("/Menu");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
 
   const [showPassword, setShowPassword] = useState(false);
 
@@ -12,11 +32,11 @@ const Login = () => {
   return (
     <div className='grid w-full h-screen place-items-center bg-gray-100'>
       <div className="flex w-full max-w-6xl bg-white shadow-[0_0_50px_5px_rgba(0,0,0,0.5)] rounded-xl overflow-hidden">
-
+        <Toaster position='top-right' />
         <div className='w-full md:w-1/2 px-20 pt-40 pb-28 flex flex-col justify-center'>
             <h2 className='text-3xl font-medium text-gray-800 mb-14 -mt-10 text-center'>Se connexion</h2>
 
-            <form className='space-y-10'>
+            <form onSubmit={handleSubmit} className='space-y-10'>
 
               <div className='space-y-8'>
                 <div className="relative">
@@ -25,6 +45,9 @@ const Login = () => {
                     <div className='ml-2 h-5 w-2 border-l-2 border-gray-400'></div>
                   </div>
                   <input
+                    name='email'
+                    value={form.email}
+                    onChange={handleChange}
                     type="text"
                     placeholder="Entrer votre email ici"
                     className="w-full pl-16 pr-4 py-4 border-l-5 border-blue-700 rounded-sm shadow-[0_0_14px_4px_rgba(0,0,0,0.2)] focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
@@ -37,6 +60,9 @@ const Login = () => {
                     <div className='ml-2 h-5 w-2 border-l-2 border-gray-400'></div>
                   </div>
                   <input
+                    name='password'
+                    value={form.password}
+                    onChange={handleChange}
                     type={ showPassword ? "text" : "password" }
                     placeholder="Entrer votre mot de passe ici"
                     className="w-full pl-16 pr-4 py-4 border-l-5 border-blue-700 rounded-sm shadow-[0_0_14px_4px_rgba(0,0,0,0.2)] focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
@@ -69,9 +95,11 @@ const Login = () => {
 
               <p className='flex justify-center items-center'>
                 Vous n'avez pas de compte?
-                <a href="#" className='text-blue-600 hover:text-blue-400'>
+                <span 
+                 onClick={() => navigate("/register")}
+                 className='text-blue-600 hover:text-blue-400'>
                   S'inscrire
-                </a> 
+                </span> 
               </p>
             </form>
 
@@ -90,6 +118,7 @@ const Login = () => {
               Entrez votre détails et commencez la journée avec nous
             </p>
             <button
+              onClick={() => navigate("/register")}
               type="button"
               className="px-8 py-3 border-2 border-white text-white font-medium rounded-full 
                          hover:bg-white hover:text-blue-600 transition duration-300 backdrop-blur-sm"
@@ -104,4 +133,3 @@ const Login = () => {
   )
 }
 
-export default Login
