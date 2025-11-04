90f3064f51c5244d39017e1a6375ac2e8bddebef
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-05 01:50:56 +0300

    feat: EncoursSlice.js

---
+++ src/redux/slices/user/EnCoursSlice.js
@@ -0,0 +1,44 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { EnCoursService } from "../../../services/EnCoursService";
+import toast from "react-hot-toast";
+
+
+export const fetchEnCours = createAsyncThunk(
+  "emprunts/fetchEnCours",
+  async (_, { rejectWithValue }) => {
+    try {
+      const data = await EnCoursService.getUserEmpruntsInProgress();
+      return data;
+    } catch (error) {
+      toast.error("Erreur lors du chargement des emprunts en cours");
+      return rejectWithValue(error.response?.data || error.message);
+    }
+  }
+);
+
+const enCoursSlice = createSlice({
+  name: "EnCours",
+  initialState: {
+    items: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchEnCours.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchEnCours.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchEnCours.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
+});
+
+export default enCoursSlice.reducer;
