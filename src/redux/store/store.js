9bdecd8c557989ced6dc70d830e3ffcd8926223d
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-04 21:26:23 +0300

    feat: ajout l'equipementReducer dans le store

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
+import equipementReducer from "../slices/user/equipementSlice";
 
 export const store = configureStore({
     reducer: { 
-        auth: authReducer 
+        auth: authReducer,
+        equipements: equipementReducer,
     },
 });
