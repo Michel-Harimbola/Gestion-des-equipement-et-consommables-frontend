f4224b5b4ed2e391cf539c1456c444900a13a3df
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-04 21:23:11 +0300

    feat: ajout equipementSlice.js

---
+++ src/redux/slices/user/equipementSlice.js
@@ -0,0 +1,42 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { equipementService } from "../../../services/equipementsService";
+import toast from "react-hot-toast";
+
+export const fetchEquipements = createAsyncThunk(
+  "equipements/fetchAll",
+  async (_, { rejectWithValue }) => {
+    try {
+        const data = await equipementService.getAll();
+        return data;
+    } catch (error) {
+        toast.error("Erreur lors du chargement des équipements");
+        return rejectWithValue(error.response?.data || error.message);
+    }
+  }  
+);
+
+const equipementSlice = createSlice({
+    name: "equipements",
+    initialState: {
+        items: [],
+        loading: false,
+        error: null,
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchEquipements.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(fetchEquipements.fulfilled, (state, action) => {
+                state.loading = false;
+                state.items = action.payload;
+            })
+            .addCase(fetchEquipements.rejected, (state, action) => {
+                state.loading = false;
+                state.error = action.payload;
+            });
+    },
+});
+
+export default equipementSlice.reducer;
