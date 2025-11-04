d1538526e60db89947f9e1af1712ec6944acc844
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-05 00:48:08 +0300

    feat: importation empruntReducer dans store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
 import equipementReducer from "../slices/user/equipementSlice";
+import empruntReducer from "../slices/user/empruntSlice";
 
 export const store = configureStore({
     reducer: { 
         auth: authReducer,
         equipements: equipementReducer,
+        empruts: empruntReducer,
     },
 });
