33a9989a9e56f705e708d09c5acd7fa61f2e6b90
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-04 21:30:23 +0300

    feat: ajout Toaster dans main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReatDOM from "react-dom/client";
 import App from "./App";
 import AppProviders from "./providers/AppProviders";
+import { Toaster } from "react-hot-toast";
 import "./index.css";
 
 ReatDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AppProviders>
+      <Toaster position="top-right" />
       <App />
     </AppProviders>
   </React.StrictMode>
