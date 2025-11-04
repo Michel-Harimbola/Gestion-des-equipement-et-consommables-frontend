32e1c87a626e33d36c40ea339de54bd6a8031e2c
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-05 01:52:39 +0300

    feat: importation enCoursSlice dans store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../slices/authSlice";
 import equipementReducer from "../slices/user/equipementSlice";
 import empruntReducer from "../slices/user/empruntSlice";
+import enCoursSlice from "../slices/user/EnCoursSlice";
 
 export const store = configureStore({
     reducer: { 
         auth: authReducer,
         equipements: equipementReducer,
-        empruts: empruntReducer,
+        emprunts: empruntReducer,
+        enCours: enCoursSlice,
     },
 });
