04be1b0fe941009cf9b6f34dbe601e0dd9d8eddf
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-04 21:31:43 +0300

    feat: ajout Equipements dans AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Login from "../pages/auth/Login";
-import Register from "../pages/auth/Register";
-import UserDashboard from "../pages/user/UserDashboard";
 import ProctectedRoute from "../components/shared/ProtectedRoute";
 import DashboardLayout from "../layouts/DashboardLayout";
+import Register from "../pages/auth/Register";
+import Login from "../pages/auth/Login";
+import UserDashboard from "../pages/user/UserDashboard";
+import Equipements from "../pages/user/Equipements";
 
 export default function AppRoutes() {
     return (
@@ -14,7 +15,7 @@ export default function AppRoutes() {
                 <Route path="/register" element={<Register />} />
                 <Route element={<ProctectedRoute><DashboardLayout /></ProctectedRoute>}>
                   <Route path="/userDashboard" element={ <UserDashboard /> } />
-                  <Route path="/dashboard/profile" />
+                  <Route path="/userDashboard/Equipements" element={ <Equipements /> } />
                   <Route path="/dashboard/settings" />
                 </Route>
             </Routes>
