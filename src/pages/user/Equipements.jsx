97cdee3778e7c7bfd12be0cb8fc00b9351eb7714
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-05 00:11:49 +0300

    fix: Erreur lors du chargement des équipements, feat: améliorer le front

--- src/pages/user/Equipements.jsx
+++ src/pages/user/Equipements.jsx
@@ -9,7 +9,7 @@ export default function Equipements() {
 
     useEffect(() => {
         dispatch(fetchEquipements());
-    }), [dispatch];
+    }, [dispatch]);
 
     return (
       <div className="">
@@ -18,15 +18,31 @@ export default function Equipements() {
           {loading ? (
             <GlobalLoader />
           ) : (
-            <div className="flex flex-row space-x-16 my-13">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 space-x-16 my-13">
                 {items.map((eq) => (
-                    <div className="flex flex-col items-center space-y-5 bg-gray-50 shadow-[0_0_30px_2px_rgba(0,0,0,0.1)] p-10 rounded-3xl">
+                    <div 
+                      key={eq.id}
+                      className="flex flex-col items-center space-y-5 bg-white shadow-[0_0_30px_2px_rgba(0,0,0,0.1)] py-8 w-[290px] rounded-3xl">
                       <div>
-                          <h1 className="font-semibold text-lg">Nom de l'équipement:</h1>
-                          <h1 className="text-xl font-semibold">eq.nom</h1>
+                          <h1 className="font-semibold text-lg text-center">Nom de l'équipement:</h1>
+                          <h1 className="text-xl font-semibold text-center">{eq.nom}</h1>
                       </div>
-                      <p className="text-green-600 text-center">eq.etat</p>
-                      <button className="bg-blue-600 text-white font-bold rounded-3xl px-4 py-2">Emprunter</button>
+                      
+                      <div className="flex flex-row justify-center items-center space-x-2">
+                        <div 
+                            className={`w-4 h-4 rounded-full ${eq.etat !== "Disponible"? "bg-red-600" : "bg-green-500"}`}
+                        >
+                        </div>
+                        <p className="text-center text-gray-600 font-normal">
+                          {eq.etat}
+                        </p>
+                      </div>
+                      
+                      <button 
+                        disabled={ eq.etat !== "Disponible" }
+                        className="bg-blue-600 text-white font-bold rounded-3xl px-4 py-2">
+                        Emprunter
+                      </button>
                     </div>
                 ))}
             </div>
