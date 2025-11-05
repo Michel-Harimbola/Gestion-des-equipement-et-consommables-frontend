f833a184cedc979cf28c5fe6fb22ff12d1f8911d
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-05 15:44:16 +0300

    feat: ajout filtrage dans Equipements.jsx

--- src/pages/user/Equipements.jsx
+++ src/pages/user/Equipements.jsx
@@ -1,28 +1,62 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { fetchEquipements } from "../../redux/slices/user/equipementSlice";
 import GlobalLoader from "../../components/shared/GlobalLoader";
 
+
 export default function Equipements() {
     const dispatch = useDispatch();
     const { items, loading } = useSelector((state => state.equipements));
+    const [filter, setFilter] = useState("all");
 
+    
     useEffect(() => {
-        dispatch(fetchEquipements());
+      dispatch(fetchEquipements());
     }, [dispatch]);
+    
+    const filteredItems = items.filter((eq) => {
+      if (filter == "all") return true;
+      return eq.etat == filter;
+    });
 
     return (
       <div className="">
-        <div className="mt-36 mx-10">
-          <h1 className="text-3xl font-semibold">Tous les équipements</h1> 
+        <div className="mt-24 ml-8">
+          <h1 className="text-4xl -ml-2 font-bold">Tous les équipements</h1> 
+
+          <div className="flex space-x-5 mt-10">
+            <button
+              onClick={() => setFilter("all")}
+              className={`px-4 py-1 rounded-lg font-semibold ${filter == "all" ? "bg-black text-white" : "bg-gray-200"}`}
+            >
+              Tous
+            </button>
+
+            <button
+              onClick={() => setFilter("Disponible")}
+              className={`px-4 py-1 rounded-lg font-semibold ${filter == "Disponible" ? "bg-black text-white" : "bg-gray-200"}`}
+            >
+              Disponible
+            </button>
+
+            <button
+              onClick={() => setFilter("Emprunter")}
+              className={`px-4 py-1 rounded-lg font-semibold ${filter == "Emprunter" ? "bg-black text-white" : "bg-gray-200"}`}
+            >
+              Emprunter
+            </button>
+          </div>
+
           {loading ? (
             <GlobalLoader />
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 space-x-16 my-13">
-                {items.map((eq) => (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-10 space-x-16 mb-13 mt-10">
+                {filteredItems.length > 0 ? (
+                  filteredItems.map((eq) => (
                     <div 
                       key={eq.id}
-                      className="flex flex-col items-center space-y-5 bg-white shadow-[0_0_30px_2px_rgba(0,0,0,0.1)] py-8 w-[290px] rounded-3xl">
+                      className="flex flex-col items-center space-y-5 bg-white shadow-[0_0_30px_2px_rgba(0,0,0,0.1)] py-8 w-[290px] rounded-3xl"
+                    >
                       <div>
                           <h1 className="font-semibold text-lg text-center">Nom de l'équipement:</h1>
                           <h1 className="text-xl font-semibold text-center">{eq.nom}</h1>
@@ -40,11 +74,19 @@ export default function Equipements() {
                       
                       <button 
                         disabled={ eq.etat !== "Disponible" }
-                        className="bg-blue-600 text-white font-bold rounded-3xl px-4 py-2">
+                        className={`text-white font-bold border border-transparent rounded-3xl px-4 py-2 ${eq.etat !== "Disponible"? 
+                              "bg-gray-400 " 
+                              : "bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 shadow-md"}
+                        `}>
                         Emprunter
                       </button>
                     </div>
-                ))}
+                  ))
+                ) : (
+                  <p className="text-gray-500 text-center col-span-full">
+                    Aucun équipement trouvé pour ce filtre.
+                  </p>
+                )}
             </div>
           )}
         </div>
