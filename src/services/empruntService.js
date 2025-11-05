ecd3f45dde4f44994789a6ab9e9f92dc0eea7410
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-06 00:46:03 +0300

    feat: ajout EncoursService et CreateEmpruntService

--- src/services/empruntService.js
+++ src/services/empruntService.js
@@ -6,3 +6,17 @@ export const empruntService = {
     return response.data;
   },
 };
+
+export const EnCoursService = {
+  async getUserEmpruntsInProgress() {
+    const response = await axiosInstance.get("/emprunt/userEmpruntsInProgress");
+    return response.data;
+  },
+};
+
+export const CreateEmpruntService = {
+  async create(empruntData) {
+    const response = await axiosInstance.post("/emprunt/create", empruntData);
+    return response.data;
+  },
+};
