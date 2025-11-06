95d22bfb08df7c191f17580ab7924ff7087e1e86
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-06 14:21:34 +0300

    feat: amélioration front navbar active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -114,7 +114,7 @@ export default function Navbar() {
                                        setIsActive(item.id);
                                        isOpen(false);                                
                                    }}
-                                   className={`ease-in-out rounded-full  duration-300 ${isActive == item.id ? "text-blue-500" : "hover:text-blue-500"}`}>
+                                   className={`ease-in-out ${isActive == item.id ? "text-blue-500 hover:text-blue-700 border-b-4 pb-6" : "hover:text-blue-500"}`}>
                                        {item.name}
                                    </Link>
                                </li>
