feaa4b6a2ff4e563f6a844aa7e4b71341d5386ad
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-08 10:39:30 +0300

    feat: admin: usersService.js

---
+++ src/services/admin/usersService.js
@@ -0,0 +1,25 @@
+import axiosInstance from "../../configs/axios";
+
+const userService = {
+  getAll: async () => {
+    const res = await axiosInstance.get("/user/");
+    return res.data;
+  },
+
+  create: async (data) => {
+    const res = await axiosInstance.post("/user/create", data);
+    return res.data;
+  },
+
+  update: async (id, data) => {
+    const res = await axiosInstance.put(`/user/${id}`, data);
+    return res.data;
+  },
+
+  remove: async (id) => {
+    const res = await axiosInstance.delete(`/user/${id}`);
+    return res.data;
+  },
+};
+
+export default userService;
