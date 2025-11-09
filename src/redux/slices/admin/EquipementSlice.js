da61286d07eb176bd33f5af07645548a5a7686f7
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-09 20:37:00 +0300

    feat: EquipementSlice.js

---
+++ src/redux/slices/admin/EquipementSlice.js
@@ -0,0 +1,82 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import equipementService from "../../../services/admin/equipementService";
+import { toast } from "react-toastify";
+
+
+export const fetchEquipements = createAsyncThunk("equipement/fetchAll", async (_, thunkAPI) => {
+  try {
+    return await equipementService.getAll();
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response?.data?.message || "Erreur lors du chargement");
+  }
+});
+
+export const createEquipement = createAsyncThunk("equipement/create", async (data, thunkAPI) => {
+  try {
+    const res = await equipementService.create(data);
+    toast.success("Equipement créé avec succès !");
+    return res;
+  } catch (error) {
+    toast.error(error.response?.data?.message || "Erreur lors de la création");
+    return thunkAPI.rejectWithValue(error.response?.data);
+  }
+});
+
+export const updateEquipement = createAsyncThunk("equipement/update", async ({ id, data }, thunkAPI) => {
+  try {
+    const res = await equipementService.update(id, data);
+    toast.success("Utilisateur mis à jour !");
+    return res;
+  } catch (error) {
+    toast.error(error.response?.data?.message || "Erreur lors de la mise à jour");
+    return thunkAPI.rejectWithValue(error.response?.data);
+  }
+});
+
+export const deleteEquipement = createAsyncThunk("equipement/delete", async (id, thunkAPI) => {
+  try {
+    await equipementService.remove(id);
+    toast.success("Utilisateur supprimé !");
+    return id;
+  } catch (error) {
+    toast.error(error.response?.data?.message || "Erreur lors de la suppression");
+    return thunkAPI.rejectWithValue(error.response?.data);
+  }
+});
+
+
+const equipementSlice = createSlice({
+  name: "equipements",
+  initialState: {
+    items: [],
+    loading: false,
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchEquipements.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(fetchEquipements.fulfilled, (state, action) => {
+        state.loading = false;
+        state.items = action.payload;
+      })
+      .addCase(fetchEquipements.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      })
+      .addCase(createEquipement.fulfilled, (state, action) => {
+        state.items.push(action.payload);
+      })
+      .addCase(updateEquipement.fulfilled, (state, action) => {
+        const index = state.items.findIndex((u) => u.id === action.payload.id);
+        if (index !== -1) state.items[index] = action.payload;
+      })
+      .addCase(deleteEquipement.fulfilled, (state, action) => {
+        state.items = state.items.filter((u) => u.id !== action.payload);
+      });
+  },
+});
+
+export default equipementSlice.reducer;
