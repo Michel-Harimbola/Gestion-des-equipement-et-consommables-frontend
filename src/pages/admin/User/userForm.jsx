d4a36f03169a433e3929fab1badfb74152072c58
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-10 09:13:35 +0300

    feat: crud users dans adminDashboard

--- src/pages/admin/User/userForm.jsx
+++ src/pages/admin/User/userForm.jsx
@@ -46,91 +46,86 @@ export default function UserForm({ onSubmit, onClose, initialData = null }) {
 
         <form onSubmit={handleSubmit} className="space-y-5">
           <input
-                    name='nom'
-                    value={form.nom}
-                    onChange={handleChange}
-                    type="text"
-                    placeholder="Votre nom"
-                    className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
-                    required
-                  /> 
-
-                  <input
-                    name='prenom'
-                    value={form.prenom}
-                    onChange={handleChange}
-                    type="text"
-                    placeholder="Votre prénom"
-                    className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
-                    required
-                  /> 
-
-                  <input
-                    name='email'
-                    value={form.email}
-                    onChange={handleChange}
-                    type="email"
-                    placeholder="Votre email"
-                    className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
-                    required
-                  /> 
-
-                <div className="relative">
-                  <input
-                    name='motDePasse'
-                    value={form.motDePasse}
-                    onChange={handleChange}
-                    type={ showPassword ? "text" : "password" }
-                    placeholder="votre mot de passe"
-                    className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
-                    required
-                  />
-                  <button
-                    type="button"
-                    onClick={togglePasswordVisibility}
-                    className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 hover:text-gray-600 focus:outline-none"
-                  >
-                    {showPassword ? (
-                      <EyeOff className="w-5 h-5" />
-                    ) : (
-                      <Eye className="w-5 h-5" />
-                    )}
-                  </button>
-                </div>
-
-                <div className="relative">
-                  <input
-                    type={ showPassword ? "text" : "password" }
-                    placeholder="Confirmer votre mot de passe"
-                    className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
-                  />
-                  <button
-                    type="button"
-                    onClick={togglePasswordVisibility}
-                    className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 hover:text-gray-600 focus:outline-none"
-                  >
-                    {showPassword ? (
-                      <EyeOff className="w-5 h-5" />
-                    ) : (
-                      <Eye className="w-5 h-5" />
-                    )}
-                  </button>
-                </div>
-
-                <div className="relative">
-                <select
-                  name='role'
-                  value={form.role}
-                  onChange={handleChange}
-                  className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm appearance-none bg-white 
-                             focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 cursor-pointer"
-                >
-                  <option value="" disabled>Choisir un rôle</option>
-                  <option value="utilisateurSimple">Utilisateur Simple</option>
-                  <option value="responsableRH">Responsable RH</option>
-                  <option value="admin">Admin</option>
-                </select>
-              </div>
+            name='nom'
+            value={form.nom}
+            onChange={handleChange}
+            type="text"
+            placeholder="Votre nom"
+            className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
+            required
+          /> 
+          <input
+            name='prenom'
+            value={form.prenom}
+            onChange={handleChange}
+            type="text"
+            placeholder="Votre prénom"
+            className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
+            required
+          /> 
+          <input
+            name='email'
+            value={form.email}
+            onChange={handleChange}
+            type="email"
+            placeholder="Votre email"
+            className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
+            required
+          /> 
+          <div className="relative">
+            <input
+              name='motDePasse'
+              value={form.motDePasse}
+              onChange={handleChange}
+              type={ showPassword ? "text" : "password" }
+              placeholder="votre mot de passe"
+              className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
+              required
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 hover:text-gray-600 focus:outline-none"
+            >
+              {showPassword ? (
+                <EyeOff className="w-5 h-5" />
+              ) : (
+                <Eye className="w-5 h-5" />
+              )}
+            </button>
+          </div>
+          <div className="relative">
+            <input
+              type={ showPassword ? "text" : "password" }
+              placeholder="Confirmer votre mot de passe"
+              className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
+            />
+            <button
+              type="button"
+              onClick={togglePasswordVisibility}
+              className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-400 hover:text-gray-600 focus:outline-none"
+            >
+              {showPassword ? (
+                <EyeOff className="w-5 h-5" />
+              ) : (
+                <Eye className="w-5 h-5" />
+              )}
+            </button>
+          </div>
+          <div className="relative">
+            <select
+              name='role'
+              value={form.role}
+              onChange={handleChange}
+              className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm appearance-none bg-white 
+                         focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 cursor-pointer"
+            >
+              <option value="" disabled>Choisir un rôle</option>
+              <option value="utilisateurSimple">Utilisateur Simple</option>
+              <option value="responsableRH">Responsable RH</option>
+              <option value="admin">Admin</option>
+            </select>
+          </div>
 
           <div className="flex justify-end gap-3 pt-2">
             <button
