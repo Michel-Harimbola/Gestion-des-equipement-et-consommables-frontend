0a953db36bc6219d4a32f1bdbabfb54c1f3d2a69
Harimbola Michel <harimbolajeanmichel13012005@gmail.com>
2025-11-10 09:16:38 +0300

    feat: crud equipement dans adminDashboard

--- src/pages/admin/Equipement/Equipement.jsx
+++ src/pages/admin/Equipement/Equipement.jsx
@@ -1,12 +1,117 @@
-import Main from "../../../ui/Main"
-
-
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchEquipements, createEquipement, deleteEquipement, updateEquipement } from "../../../redux/slices/admin/EquipementSlice";
+import EquipementForm from "./equipementForm";
+import { FiDelete } from "react-icons/fi";
+import { GrUpdate } from "react-icons/gr";
 export default function Equipement() {
+    const dispatch = useDispatch();
+    const { items, loading } = useSelector((state) => state.equipements);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [selectedEquipement, setSelectedEquipement] = useState(null);
+
+    useEffect(() => {
+      dispatch(fetchEquipements());
+    }, [dispatch]);
+
+    const handleAdd = () => {
+      setSelectedEquipement(null);
+      setIsModalOpen(true);
+    };
+
+    const handleEdit = (equipement) => {
+      setSelectedEquipement(equipement);
+      setIsModalOpen(true);
+    };
+
+    const handleDelete = (id) => {
+      if (window.confirm("Voulez-vous vraiment supprimer cet équipement ?")) {
+        dispatch(deleteEquipement(id));
+      }
+    };
+
+    const handleSubmit = (formData) => {
+      if (selectedEquipement) {
+        dispatch(updateEquipement({ id: selectedEquipement.id, data: formData }));
+      } else {
+        dispatch(createEquipement(formData));
+      }
+      setIsModalOpen(false);
+    };
+
     return (
-        <div className="">
-            <Main>
-                Equipement
-            </Main>
+        <div className="ml-[300px] mr-8 mt-22 h-full ">
+            <div className="flex justify-end">
+                <button
+                    onClick={handleAdd}
+                    className=" gap-2 px-4 py-2 border border-transparent text-lg font-semibold rounded-lg text-white
+                    bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 shadow-md"       
+                >
+                    Ajouter <span className="text-2xl font-bold">+</span>
+                </button>
+            </div>
+
+            <div className="overflow-x-auto rounded-lg mt-10">
+                {loading ? (
+                  <p>Chargement...</p>
+                ) : (
+                  <table className="min-w-full text-lg text-gray-700">
+                    <thead className="bg-sky-500 text-white">
+                      <tr>
+                        <th className="py-3 px-4 text-left">ID</th>
+                        <th className="py-3 px-4 text-left">Nom</th>
+                        <th className="py-3 px-4 text-left">Type</th>
+                        <th className="py-3 px-4 text-left">Etat</th>
+                        <th className="py-3 px-4 text-left">Actions</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {items.map((equipement, index) => (
+                        <tr 
+                            key={equipement.id} 
+                            className="odd:bg-white even:bg-gray-100 hover:bg-gray-200 transition-colors"
+                        >
+                            <td className="py-2 px-4 font-medium">{index + 1}</td>
+                            <td className="py-2 px-4">
+                              {equipement.nom}
+                            </td>
+                            <td className="py-2 px-4">
+                              {equipement.type}
+                            </td>
+                            <td className="py-2 px-4">
+                              <span
+                                className={`px-3 py-1 rounded-full text-lg font-semibold ${
+                                  equipement.etat === "Disponible"
+                                  ? "bg-green-100 text-green-700"
+                                    : "bg-yellow-100 text-yellow-700"
+                                }`}
+                              >
+                                {equipement.etat}
+                              </span>
+                            </td>
+                            <td className="p-2 space-x-8 flex">
+                                <button onClick={() => handleEdit(equipement)} className="text-xl hover:text-white">
+                                   <GrUpdate />
+                                </button>
+                                <button onClick={() => handleDelete(equipement.id)} className="text-red-500 text-2xl hover:text-white">
+                                   <FiDelete />
+                                </button>
+                            </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                )}
+            </div>
+
+            {isModalOpen && (
+                <EquipementForm
+                  onSubmit={handleSubmit}
+                  onClose={() => setIsModalOpen(false)}
+                  initialData={selectedEquipement}
+                  items={items}
+                />
+            )}
         </div>
-    )
+    );
 }
---
+++ src/pages/admin/Equipement/equipementForm.jsx
@@ -0,0 +1,90 @@
+import { useState, useEffect } from "react";
+
+
+export default function EquipementForm({ onSubmit, onClose, initialData = null }) {
+  const [form, setForm] = useState({
+    nom: "",
+    type: "",
+    etat: ""
+  });
+
+  useEffect(() => {
+    if (initialData) {
+      setForm({
+        nom: initialData.nom || "",
+        type: initialData.type || "",
+        etat: initialData.etat || "",
+      });
+    }
+  }, [initialData]);
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit(form);
+  };
+
+  return (
+    <div className="fixed inset-0 bg-black/40 flex items-center justify-center z-50">
+      <div className="bg-gray-100 rounded-lg p-6 w-full max-w-md shadow-lg">
+        <h2 className="text-2xl font-semibold mb-5">
+          {initialData ? "Modifier l'équipement" : "Ajouter un équipement"}
+        </h2>
+
+        <form onSubmit={handleSubmit} className="space-y-5">
+          <input
+            name='nom'
+            value={form.nom}
+            onChange={handleChange}
+            type="text"
+            placeholder="Votre nom"
+            className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
+            required
+          /> 
+          <input
+            name='type'
+            value={form.type}
+            onChange={handleChange}
+            type="text"
+            placeholder="Votre type"
+            className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm bg-white focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150"
+            required
+          /> 
+          <div className="relative">
+            <select
+              name='role'
+              value={form.etat}
+              onChange={handleChange}
+              className="w-full pl-5 pr-4 py-3 border-l-5 border-blue-700 rounded-sm appearance-none bg-white 
+                         focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-150 cursor-pointer"
+            >
+              <option value="Disponible">Disponible</option>
+              <option value="EnMaintenance">En Maintenance</option>
+              <option value="Emprunter">Emprunter</option>
+            </select>
+          </div>
+          <div className="flex justify-end gap-3 pt-2">
+            <button
+              type="button"
+              onClick={onClose}
+              className="gap-2 px-4 py-2 border border-transparent text-lg font-semibold rounded-lg text-white
+                    bg-gray-400 hover:bg-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400 transition duration-150 shadow-md"
+            >
+              Annuler
+            </button>
+            <button
+              type="submit"
+              className="gap-2 px-4 py-2 border border-transparent text-lg font-semibold rounded-lg text-white
+                    bg-blue-500 hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition duration-150 shadow-md"
+            >
+              {initialData ? "Mettre à jour" : "Créer"}
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
